53c54a738814aaf8a53adbb2408f15c26b2b3099
mai_b <maim181@outlook.com>
2023-04-03 00:00:07 +0100

    Deleted commented out html and updated js file

--- js/app.js
+++ js/app.js
@@ -1,37 +1,3 @@
-/* let toggle = document.querySelector("#toggle");
-console.log(toggle); */
-// let switchToggle = document.querySelector(".switch");
-// console.log(switchToggle);
-/* let ball = document.querySelector(".ball");
-console.log(ball);
-let pricingToggle = document.querySelector(".pricing-toggle");
-console.log(pricingToggle);
-let container = document.querySelector(".container");
-console.log(container);
-
-toggle.addEventListener("click", function () {
-  if (container.classList.contains("monthy")) {
-    container.classList.remove("monthly");
-    container.classList.add("annual");
-    ball.classList.add("move-left");
-  } else {
-    container.classList.remove("annual");
-    ball.classlist.remove("move-left");
-    container.classList.add("monthy");
-  }
-}); */
-
-/* toggle.addEventListener("click", function () {
-  if (container.classList.contains("monthy")) {
-    container.clasList.remove("monthy");
-    ball.classList.add("move-left");
-  } else {
-    container.classList.remove("annual");
-    container.classList.add("monthy");
-    ball.classlist.remove("move-left");
-  }
-}); */
-
 let toggle = document.querySelector("#toggle");
 console.log(toggle);
 let monthly = document.querySelectorAll(".monthly");
@@ -39,10 +5,12 @@ let annual = document.querySelectorAll(".annual");
 
 toggle.addEventListener("click", function () {
   if (toggle.checked) {
-    annual.forEach(price-option => price-option.style.display = "none");
-    monthly.forEach(price-option => price-option.style.display = "none");
+    annual.forEach((priceOption) => (priceOption.style.display = "flex"));
+    monthly.forEach((priceOption) => (priceOption.style.display = "none"));
     // console.log("checked");
   } else {
     // console.log("unchecked");
+    annual.forEach((priceOption) => (priceOption.style.display = "none "));
+    monthly.forEach((priceOption) => (priceOption.style.display = "block"));
   }
 });
